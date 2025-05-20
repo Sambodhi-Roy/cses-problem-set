4f50bda79700a5d96bc5adb1e06b84e3a5537e3f
Sambodhi-Roy <sambodhiroy2005@gmail.com>
2025-05-20 20:43:30 +0530

    Reapply "Added a question"
    This reverts commit e35bb7e9b58e55e58c64f9c3e5371c2789e94a4e.

--- range_queries/Static_Range_Sum_Queries.cpp
+++ range_queries/Static_Range_Sum_Queries.cpp
@@ -1,77 +1,78 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 inline int power(int a, int b)
 {
-	int x = 1;
-	while (b)
-	{
-		if (b & 1) x *= a;
-		a *= a;
-		b >>= 1;
-	}
-	return x;
+    int x = 1;
+    while (b)
+    {
+        if (b & 1)
+            x *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return x;
 }
 
-
 template <typename Arg1>
-void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+void __f(const char *name, Arg1 &&arg1) { cout << name << " : " << arg1 << endl; }
 template <typename Arg1, typename... Args>
-void __f (const char* names, Arg1&& arg1, Args&&... args)
+void __f(const char *names, Arg1 &&arg1, Args &&...args)
 {
-	const char* comma = strchr (names + 1, ',');
-	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+    const char *comma = strchr(names + 1, ',');
+    cout.write(names, comma - names) << " : " << arg1 << " | ";
+    __f(comma + 1, args...);
 }
 
 const int N = 200005;
 
 void solve()
 {
-	int n,q;
-	cin>>n>>q;
-	vector<int> nums(n);
-	for(auto &it:nums)	cin>>it;
+    int n, q;
+    cin >> n >> q;
+    vector<int> nums(n);
+    for (auto &it : nums)
+        cin >> it;
 
-	vector<long long> prefixSum(n);
-	prefixSum[0] = nums[0];
+    vector<long long> prefixSum(n);
+    prefixSum[0] = nums[0];
 
-	for(int i=1;i<n;i++)
-	{
-		prefixSum[i] = prefixSum[i-1] + nums[i];
-	}
+    for (int i = 1; i < n; i++)
+    {
+        prefixSum[i] = prefixSum[i - 1] + nums[i];
+    }
 
+    while (q--)
+    {
+        int a, b;
+        cin >> a >> b;
+        a--;
+        b--;
 
+        long long int sum = prefixSum[b];
+        sum -= ((a == 0) ? 0 : prefixSum[a - 1]);
 
-	while(q--)
-	{
-		int a,b;
-		cin>>a>>b;
-		a--;
-		b--;
-
-		long long int sum = prefixSum[b];
-		sum -= ((a==0)? 0: prefixSum[a-1]);
-
-		cout<<sum<<endl;
-	}
+        cout << sum << endl;
+    }
 }
-
 
 int32_t main()
 {
 
-	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
 #ifndef ONLINE_JUDGE
-	freopen("input.txt","r",stdin);
-	freopen("output.txt","w",stdout);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
 #endif
 
-	clock_t z = clock();
+    clock_t z = clock();
 
-	solve();
+    solve();
 
-	cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC);
+    cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC);
 
-	return 0;
+    return 0;
 }
